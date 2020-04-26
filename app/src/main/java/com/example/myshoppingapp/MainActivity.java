2479e0139249b6fbddb4c5958e1acebd0e3dc749
YogeshPc <developerhappy123@gmail.com>
2020-04-26 17:09:25 +0530

    Chapter 1

--- app/src/main/java/com/example/myshoppingapp/MainActivity.java
+++ app/src/main/java/com/example/myshoppingapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myshoppingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -10,11 +11,16 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference myRef;
+    private Button joinNowButton,loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        joinNowButton=(Button)findViewById(R.id.main_join_now_btn);
+        loginButton=(Button)findViewById(R.id.main_login_btn);
+
 
     }
 }
